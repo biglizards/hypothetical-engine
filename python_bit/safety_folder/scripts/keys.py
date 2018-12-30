7a710215270353b030f8a196029da7d2e3862fe9
biglizards <22501070+biglizards@users.noreply.github.com>
2018-12-30 14:48:57 +0000

    half add scripts

---
+++ python_bit/safety_folder/scripts/keys.py
@@ -0,0 +1,20 @@
+import script
+import engine
+
+
+class CustomKeyPresses(script.Script):
+    @script.on_key_press()
+    def do_stuff(self, key, _scancode, action, _mods):
+        if action != engine.KEY_PRESS:
+            return
+
+        if key == engine.KEY_Z:
+            self.game.close()
+        if key == engine.KEY_X:
+            self.game.set_cursor_capture('disabled')
+            # prevent the camera from jumping when switching between normal and disabled without moving the mouse
+            self.game.camera.first_frame = True
+        if key == engine.KEY_C:
+            self.game.set_cursor_capture('normal')
+        if key == engine.KEY_ESCAPE:
+            self.game.select_entity(None)
