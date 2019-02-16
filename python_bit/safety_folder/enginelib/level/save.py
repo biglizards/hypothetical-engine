7a4f263c1807299ab4a2f6aaafa618ca69f84c86
biglizard <biglizard@harru.club>
2019-02-16 15:46:00 +0000

    modify save to check if its args have updated upon saving

--- python_bit/safety_folder/enginelib/level/save.py
+++ python_bit/safety_folder/enginelib/level/save.py
@@ -16,13 +16,6 @@ def custom_formatwarning(msg, *args, **kwargs):
 warnings.formatwarning = custom_formatwarning
 
 
-def handle(data):
-    output_data = {}
-    for name, value in data.items():
-        output_data[name] = handle_item(value)
-    return output_data
-
-
 # noinspection PyCallingNonCallable
 def handle_item(item):
     for base_class in inspect.getmro(item.__class__):
@@ -53,13 +46,65 @@ def handle_entity(entity):
     # after reading https://stackoverflow.com/questions/2020014/get-fully-qualified-class-name-of-an-object-in-python
     # i'm using the top answer but using __qualname__ instead
     entity_obj = {'@type': 'entity', '@class_module': entity.__class__.__module__,
-                  '@class_name': entity.__class__.__qualname__, '@args': handle_item(entity._args)}
-    entity_obj.update(handle(entity._kwargs))
-
-    # todo try to re-import the name to make sure it worked and refers to the correct thing
+                  '@class_name': entity.__class__.__qualname__,
+                  '@args': handle_entity_args(entity, entity._args)}
+    entity_obj.update(handle_entity_kwargs(entity, entity._kwargs))
+    # todo 1. raise error if arg names change
+    # todo 2. add way to change signature of entity without breaking saves
+    # todo 3. try to re-import the name to make sure it worked and refers to the correct thing (cont)
     # and/or only accept classes that can be guaranteed to be available in PYTHONPATH like the library,
     # or files within the bit of the level that gets zipped (like example_app/classes/my_custom_class.py)
     return entity_obj
+
+
+def handle_entity_args(entity, args):
+    handled_args = []
+    for i, name, initial_value in enumerate(zip(args, inspect.getfullargspec(entity.__init__).args)):
+        try:
+            handled_args.append(handle_entity_arg(entity, name, initial_value))
+        except AttributeError:
+            handled_args.append(handle_item(initial_value))
+    return args
+
+
+def handle_entity_kwargs(entity, kwargs):
+    handled_kwargs = {}
+    for name, initial_value in kwargs.items():
+        try:
+            handled_kwargs[name] = handle_entity_kwarg(entity, name, initial_value)
+        except AttributeError:
+            handled_kwargs[name] = handle_item(initial_value)
+    return handled_kwargs
+
+
+def handle_entity_arg(entity, name, initial_value):
+    """
+    if the entity has an attribute with the same name as an argument, try to take that as our value
+    instead of the initial value passed to __init__ (to account for stuff like entity.position changing)
+    raises AttributeError, which is caught by `handle_entity_args`
+    """
+
+    new_value = getattr(entity, name)
+    if not isinstance(new_value, type(initial_value)) and initial_value is not None:
+        warn(f"WARNING: value {name} of entity {entity} changed type from __init__ argument. "
+             f"Consider renaming it.")
+        return
+    return handle_item(initial_value)
+
+
+def handle_entity_kwarg(entity, name, initial_value):
+    """
+    if the entity has an attribute with the same name as an argument, try to take that as our value
+    instead of the initial value passed to __init__ (to account for stuff like entity.position changing)
+    raises AttributeError, which is caught by `handle_entity_kwargs`
+    """
+
+    new_value = getattr(entity, name)
+    if not isinstance(new_value, type(initial_value)) and initial_value is not None :
+        warn(f"WARNING: value {name} of entity {entity} changed type from __init__ argument. "
+             f"Consider renaming it.")
+        return
+    return handle_item(initial_value)
 
 
 handlers = {int: lambda x: x,
@@ -67,7 +112,7 @@ handlers = {int: lambda x: x,
             float: lambda x: x,
             bool: lambda x: x,
             type(None): lambda x: x,
-            dict: handle,
+            dict: lambda data: {name: handle_item(value) for name, value in data.items()},
             list: lambda x: [handle_item(z) for z in x],
             tuple: lambda x: [handle_item(z) for z in x],  # todo have this not be a list because that's wrong
             Entity: handle_entity,
